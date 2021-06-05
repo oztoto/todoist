46c2f67ebc2110bc380dd7497c4e6b1f5975e70e
oztoto <oztodog@gmail.com>
2021-06-05 17:19:59 +0900

    横幅調整

--- todoista/src/components/templates/App/index.js
+++ todoista/src/components/templates/App/index.js
@@ -1,12 +1,14 @@
 import "./index.module.scss";
 import TaskView from "../../organism/TaskView/index";
+import Header from "../../organism/Header/index";
 import "../../../styles/reset.scss";
 import "../../../styles/foundation.scss";
 import styles from "./index.module.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      <Header />
       <div className={styles.App__task}>
         {/* [TODO] TaskViewにclass名を割り当てたい */}
         <TaskView />
